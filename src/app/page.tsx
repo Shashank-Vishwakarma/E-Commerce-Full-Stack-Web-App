f7b00b8e9d73d3eb10b7b9a7589930e437e7f930
Shashank <shashank78680@gmail.com>
2024-11-30 22:12:17 +0530

    clerk auth setup and modal creation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import Image from "next/image";
+"use client"
+
+import Modal from "@/components/modal/modal";
 
 export default function Home() {
   return (
     <p className="">
-      Hello admin dash
+      <Modal title="test" description="test" isOpen={true} onClose={() => {}} />
     </p>
-
-    
   );
 }
